b1aaed7b43f6dc4eb8ef9b83ae6aa2a100ea1cf4
callixmiller <calmill413@gmail.com>
2023-02-03 16:23:01 -0500

    index.js passing test, bigO noted, pseudocode noted, and code explanation given

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) return true
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  This solution uses O(n^2) time complexity
 */
 
 /* 
   Add your pseudocode here
+  Iterate through an array comparing two values
+    find if two numbers add up equal the target value
+      if two numbers in array add up to target value
+        return true
 */
 
 /*
   Add written explanation of your solution here
+  hasTargetSum iterates a for loop on the array.
+  A nested loop iterates the array with an index of + 1, this prevents a number being duplicated.
+  If statement checks to see if array index vale of i + array index value of j is equal to target value.
+  If two values add up to target value, true is returned.
+  If two values do not add up to target value, false is return. 
 */
 
 // You can run `node index.js` to view these console logs
